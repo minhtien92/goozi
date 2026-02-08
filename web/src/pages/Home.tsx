54f9c7ccbc5d78392108a7e86a7899dcd4c2b337
Tiến Nguyễn Minh - TT PTPM1 <tiennm@meeyland.com>
2026-02-08 21:35:21 +0700

    auto deploy

--- web/src/pages/Home.tsx
+++ web/src/pages/Home.tsx
@@ -263,8 +263,8 @@ export default function Home() {
           </div>
 
           {/* Center: logo image - not clickable */}
-          <div className="absolute left-1/2 transform -translate-x-1/2 pointer-events-none mt-8">
-            <img src={logoHeader} alt="Goozi logo" className="h-10 md:h-12" />
+          <div className="absolute left-1/2 transform -translate-x-1/2 pointer-events-none mt-16">
+            <img src={logoHeader} alt="Goozi logo" className="h-16 md:h-20 2xl:h-24" />
           </div>
 
           {/* Right: user / login */}
